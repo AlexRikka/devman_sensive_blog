e6b9c9a1633ac9ad8844906f94c272d1fc90a418
AlexRikka <rikka.gold@mail.ru>
2024-02-10 23:51:40 +0400

    add template logic to post page fix

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ def serialize_post(post):
         "text": post.text,
         "author": post.author.username,
         "comments_amount": Comment.objects.filter(post=post).count(),
-        "image_url": post.image.url if post.image else post.image,
+        "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
     }
@@ -59,7 +59,7 @@ def post_detail(request, slug):
         "author": post.author.username,
         "comments": serialized_comments,
         'likes_amount': post.likes.count(),
-        "image_url": post.image.url if post.image else post.image,
+        "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
     }
